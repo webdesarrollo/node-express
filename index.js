49c147f1fe4af4c0687a35f8a17a33fba7c25ba0
Sergio Balanda <sbalanda@globons.com>
2021-07-29 11:21:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const requestIp = require('request-ip');
+// const requestIp = require('request-ip');
 
 const app = express()
 const port = 5000
 app.use(cors())
 app.use(bodyParser.json())
-app.use(requestIp.mw())
+// app.use(requestIp.mw())
 
 app.get('/', (req, res) =>{
      return res.send("hola")
 })
 
 app.get('/api/ip', (req, res) =>{
-    const requestIp = req.clientIp;
-    console.log("request ",req)
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log("ip ",ip);
-    console.log("requestIp ",requestIp);
-    res.status(201).json({ip:ip,requestIp:requestIp});
+    // const requestIp = req.clientIp;
+    let ip = req.headers['x-forwarded-for'] || 
+    req.connection.remoteAddress || 
+    req.socket.remoteAddress || 
+    req.connection.socket.remoteAddress;    
+
+    const sep = ip.indexOf(',') > 0 ? ',' : (ip.indexOf(' ') > 0 ? ' ' : '')
+    if (sep !== '') {
+      ip = ip.split(sep)[0]
+    }
+
+    ip = ip === '::1' ? '127.0.0.1' : ip
+
+    res.status(201).json({
+        ip: ip, 
+    });
 })
 
 const PORT = process.env.PORT || 5000
