1eed7a4574f7ab9b3943716e801c8c094d1d976a
Sergio Balanda <sbalanda@globons.com>
2021-07-29 08:25:01 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const requestIp = require('request-ip');
+
+const app = express()
+const port = 5000
+app.use(cors())
+app.use(bodyParser.json())
+app.use(requestIp.mw())
+
+app.get('/', (req, res) =>{
+     return res.send("hola")
+})
+
+app.get('/api/ip', (req, res) =>{
+    const requestIp = req.clientIp;
+    console.log("request ",req)
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log("ip ",ip);
+    console.log("requestIp ",requestIp);
+    res.status(201).json({ip:ip,requestIp:requestIp});
+})
+
+app.listen(port,()=>{
+    console.log("server puerto ",port)
+})
